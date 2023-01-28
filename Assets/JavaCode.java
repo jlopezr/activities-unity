f76cf12869f919d8fa83356ba82418c5168250de
Juan López Rubio <j.lopez.r@gmail.com>
2023-01-28 23:52:27 +0100

    Launch activities and set result

---
+++ Assets/JavaCode.java
@@ -0,0 +1,43 @@
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
+import com.unity3d.player.UnityPlayer;
+
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+
+public class JavaCode {
+    public static int add(int a, int b) {
+        return a+b;
+    }
+
+    public static void setResultAndFinish(String result) {
+        Intent resultIntent = new Intent();        
+        resultIntent.putExtra("input2", result);
+        Activity a = UnityPlayer.currentActivity;
+        a.setResult(Activity.RESULT_OK, resultIntent);
+        a.finish();
+    }
+
+    public static void launchActivity(String pkg, String activity, String data) {
+        Intent i = new Intent();
+        i.setComponent(new ComponentName(pkg, activity));
+        if(data!=null) {
+            i.putExtra("input",data);
+        }        
+        UnityPlayer.currentActivity.startActivity(i);
+    }
+
+    public static String launchActivityAndGetResult(String pkg, String activity, String data) {
+        Intent i = new Intent();
+        i.setComponent(new ComponentName(pkg, activity));
+        if(data!=null) {
+            i.putExtra("input",data);
+        }        
+        UnityPlayer.currentActivity.startActivityForResult(i, 1); // Request code is hardwired to 1
+
+
+        return null;
+    }
+}
---
+++ Assets/Startup.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Startup : MonoBehaviour
+{
+
+	public Text txt;
+
+	// Use this for initialization
+	void Start()
+	{
+        string input = Android.GetStringExtra("input");
+		if(input!=null)
+		{
+			txt.text = input;
+			Debug.Log(input);
+		} else
+		{
+            txt.text = "No input extra";
+            Debug.Log("No input extra");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+	{
+			
+	}
+}
+
